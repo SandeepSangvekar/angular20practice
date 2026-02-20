2109e0f462a364647d5d38f1f3d94be15917520b
SandeepSangvekar <sandip.sangvekar@gmail.com>
2026-02-20 17:43:57 +0530

    reactive form valuechanges, astatuschanges

--- src/app/reactive-form/reactive-form.ts
+++ src/app/reactive-form/reactive-form.ts
@@ -27,13 +27,60 @@ export class ReactiveForm {
       lName: ['', Validators.required],
       email: ['', Validators.compose([Validators.required, Validators.email])],
       contactNo: [9298383383, Validators.required],
-
+    })
+    // using valueChanges on specific formControl
+    this.regForm.get('fName')?.valueChanges.subscribe(firstName =>{
+      console.log('firstName:', firstName);
+    })
+    // using valueChanges on formGroup
+    this.regForm.valueChanges.subscribe(formData =>{
+      console.log('firstName:', formData.fName);
+      console.log('lastName:', formData.lName);
+      console.log('formData:', formData); // to get all formGroup data
+    });
+    // to check status using statusChanges
+    this.regForm.get('fName')?.statusChanges.subscribe(fnameStatus =>{
+      console.log('fName status:', fnameStatus);
+    });
+    // to check status for all formGroup
+    this.regForm.statusChanges.subscribe(formDataStatus =>{
+      console.log('formDataStatus:', formDataStatus);
+      
     })
   }
   regForm!:FormGroup;
 
+  reset(){
+    // this.regForm.reset();
+    // this.regForm.reset({
+    //   fName: 'Sandeep'
+    // })
+    this.regForm.reset({
+      fName: this.regForm.get('fName')?.value,
+    })
+  }
+
   onSubmit(formData: FormGroup){
     console.log(formData.value);
     console.log(formData.valid);
+    console.log(formData.get('fName')?.value);
+    console.log(formData.get('lName')?.value);
   }
-}
+  fillData(){
+    // to fill complete formGroup use setVaule()
+    // this.regForm.setValue({
+    //   fName: 'Sandeep',
+    //   lName: 'Sangvekar',
+    //   email: 'text@gmail.com',
+    //   contactNo: '9999929999',
+    // })
+    // tto fill specific formGroup control use setVaule()
+    this.regForm.patchValue({
+      fName: 'Vipul',
+      lName: 'Shah',
+      // email: '',
+      contactNo: 9292922929
+    })
+  }
+  
+}   
